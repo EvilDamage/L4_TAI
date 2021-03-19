a515cb94d0dc2cb4e0456df15dfe44cd8e32fefc
Przemysław Kuca <evildemage@gmail.com>
2021-03-19 17:49:22 +0100

    quiz

--- js/script.js
+++ js/script.js
@@ -172,33 +172,75 @@ let preQuestions =
         }];
 
 let next = document.querySelector('.next');
+let previous = document.querySelector('.previous');
 
 let question = document.querySelector('.question');
+let questionIndex = document.querySelector('.question-index');
 let answers = document.querySelectorAll('.list-group-item');
+
+let list = document.querySelector('.list');
+let results = document.querySelector('.results');
+
+let userScorePoint = document.querySelector('.userScorePoint');
+let average = document.querySelector('.average');
 
 let pointsElem = document.querySelector('.score');
 let restart = document.querySelector('.restart');
 let index = 0;
 let points = 0;
+let activeAnswers = true;
+let obj = {answers: []};
 
+
+setQuestion(index)
 for (let i = 0; i < answers.length; i++) {
     answers[i].addEventListener('click', doAction);
 }
 
 function doAction(event) {
     //event.target - Zwraca referencję do elementu, do którego zdarzenie zostało pierwotnie wysłane.
-    if (event.target.innerHTML === preQuestions[index].correct_answer) {
-        points++;
-        pointsElem.innerText = points;
-        markCorrect(event.target);
+    if (activeAnswers) {
+        if (event.target.innerHTML === preQuestions[index].correct_answer) {
+            points++;
+            pointsElem.innerText = points;
+            obj.answers[index] = event.target.innerHTML
+            markCorrect(event.target);
+        } else {
+            obj.answers[index] = event.target.innerHTML
+            markInCorrect(event.target);
+        }
+        disableAnswers();
     }
-    else {
-        markInCorrect(event.target);
-    }
-    disableAnswers();
 }
 
+function markCorrect(e) {
+    e.classList.add('correct')
+}
 
+function markInCorrect(e) {
+    e.classList.add('incorrect')
+}
+
+function activateAnswers() {
+    activeAnswers = true;
+}
+
+function disableAnswers() {
+    activeAnswers = false;
+}
+
+function setQuestion(index) {
+    question.innerHTML = preQuestions[index].question
+    questionIndex.innerHTML = (index + 1) + ' z ' + preQuestions.length
+    for (let i = 0; i < 4; i++) {
+        if (i < preQuestions[index].answers.length) {
+            answers[i].innerHTML = preQuestions[index].answers[i];
+            answers[i].style.display = 'block';
+        } else {
+            answers[i].style.display = 'none';
+        }
+    }
+}
 
 restart.addEventListener('click', function (event) {
     event.preventDefault();
@@ -211,4 +253,53 @@ restart.addEventListener('click', function (event) {
     activateAnswers();
     list.style.display = 'block';
     results.style.display = 'none';
+});
+
+next.addEventListener('click', function (event) {
+    event.preventDefault();
+    index++;
+    if (index < preQuestions.length) {
+        localStorage.setItem("test", JSON.stringify(obj));
+        activateAnswers();
+
+        setQuestion(index)
+        preQuestions[index].answers.map((answer, i) => {
+            answers[i].classList.remove('correct')
+            answers[i].classList.remove('incorrect')
+        })
+    }
+
+    if (index === preQuestions.length) {
+        list.style.display = 'none';
+        results.style.display = 'block';
+
+        userScorePoint.innerHTML = points;
+        average.innerHTML = (points/preQuestions.length * 100) + '%'
+
+    }
+});
+
+previous.addEventListener('click', function (event) {
+    event.preventDefault();
+    if (index > 0) {
+        let obj = JSON.parse(localStorage.getItem("test"));
+        index--;
+        disableAnswers();
+        setQuestion(index)
+
+        preQuestions[index].answers.map((answer, i) => {
+            if (answer === obj.answers[index]) {
+                if (obj.answers[index] === preQuestions[index].correct_answer) {
+                    answers[i].classList.add('correct')
+                } else {
+                    answers[i].classList.add('incorrect')
+                }
+
+            } else {
+                answers[i].classList.remove('correct')
+                answers[i].classList.remove('incorrect')
+            }
+        })
+    }
+
 });
